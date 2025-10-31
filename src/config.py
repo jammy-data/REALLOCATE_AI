ef48f4f1d8eca4505923ae694e035b73b5b98506
“cyclingMoritz” <luca.liebscht@estudiantat.upc.edu>
2025-10-31 12:30:10 +0100

    New files with config

--- src/config.py
+++ src/config.py
@@ -15,6 +15,8 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 # Data directories
 DATA_DIR = PROJ_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
+INTERIM_DATA_DIR = DATA_DIR / "interim"
+
 PROCESSED_DATA_DIR = DATA_DIR / "processed"
 EXTERNAL_DATA_DIR = DATA_DIR / "external"
 
